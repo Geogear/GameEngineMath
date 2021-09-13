ee3dbf533481441bdb50d3da54189de49842ff05
unknown <mustafaenesbilgic@hotmail.com>
2021-09-13 23:09:39 +0300

    Fix minor errors.

--- Lib/Vector.h
+++ Lib/Vector.h
@@ -18,6 +18,7 @@ typedef enum
 
 float Vec3GetMagnitude(Vector3D *vec3);
 Vector3D Vec3NormalizeNew(Vector3D *vec3);
+void Vec3Normalize(Vector3D *vec3);
 Vector3D Vec3ScalarOperationNew(Vector3D *vec3, Operation operation, float scalar);
 void Vec3ScalarOperation(Vector3D *vec3, Operation operation, float scalar);
 
--- Src/Vector.c
+++ Src/Vector.c
@@ -9,7 +9,7 @@ Vec3NormalizeNew(Vector3D *vec3)
 {
     Vector3D normalized = {};
     float reverseMagnitude = InverseSquareRoot(vec3->x*vec3->x + vec3->y*vec3->y + vec3->z*vec3->z);
-    Vec3ScalarOperation(&normalized, Operation.Mul, reverseMagnitude);
+    Vec3ScalarOperation(&normalized, Mul, reverseMagnitude);
     return normalized;
 }
 
@@ -17,7 +17,7 @@ void
 Vec3Normalize(Vector3D *vec3)
 {
     float reverseMagnitude = InverseSquareRoot(vec3->x*vec3->x + vec3->y*vec3->y + vec3->z*vec3->z);
-    Vec3ScalarOperation(vec3, Operation.Mul, reverseMagnitude);
+    Vec3ScalarOperation(vec3, Mul, reverseMagnitude);
 }
 
 Vector3D
@@ -26,23 +26,23 @@ Vec3ScalarOperationNew(Vector3D *vec3, Operation operation, float scalar)
     Vector3D operated = {};
     switch (operation)
     {
-        case Operation.Mul:
+        case Mul:
             operated.x = vec3->x * scalar;
             operated.y = vec3->y * scalar;
             operated.z = vec3->z * scalar;
             break;
-        case Operation.Div:
+        case Div:
             operated.x = vec3->x / scalar;
             operated.y = vec3->y / scalar;
             operated.z = vec3->z / scalar;
             break;
-        case Operation.Negate:
+        case Negate:
             operated.x = vec3->x * -1;
             operated.y = vec3->y * -1;
             operated.z = vec3->z * -1;
             break;        
         default:
-            return NULL;
+            ErrorExit("Invalid operation with a vector and a scalar.\n");
     }
     return operated;
 }
@@ -52,17 +52,17 @@ Vec3ScalarOperation(Vector3D *vec3, Operation operation, float scalar)
 {
     switch (operation)
     {
-        case Operation.Mul:
+        case Mul:
             vec3->x *= scalar;
             vec3->y *= scalar;
             vec3->z *= scalar;
             break;
-        case Operation.Div:
+        case Div:
             vec3->x /= scalar;
             vec3->y /= scalar;
             vec3->z /= scalar;
             break;
-        case Operation.Negate:
+        case Negate:
             vec3->x *= -1;
             vec3->y *= -1;
             vec3->z *= -1;
